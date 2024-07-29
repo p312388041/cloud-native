f21a20dc79091e1eff9abb4b15481356b2255656
p312388041 <312388041@qq.com>
2024-07-29 15:09:27 +0900

    网关改名

--- microservice-gateway/src/main/java/server/EurekaServer.java
+++ microservice-gateway/src/main/java/server/EurekaServer.java
@@ -1,11 +1,9 @@
 package server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
  
-@SpringBootApplication
-@EnableEurekaServer
+@SpringBootApplication 
 public class EurekaServer {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer.class, args);
