9c0c86769b5d9e6cbf1f66178d49d350b7f54004
p312388041 <312388041@qq.com>
2024-10-01 09:44:21 +0900

    update

--- microservice-gateway/src/main/java/com/chong/study/GateWay.java
+++ microservice-gateway/src/main/java/com/chong/study/GateWay.java
@@ -1,11 +1,22 @@
 package com.chong.study;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
- 
-@SpringBootApplication  
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class GateWay {
     public static void main(String[] args) {
         SpringApplication.run(GateWay.class, args);
+    }
+
+    @Bean
+    public RouteLocator redirectRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_router", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .build();
     }
 }
